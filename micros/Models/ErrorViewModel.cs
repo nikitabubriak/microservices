9bc7efb2f3f84543e397d1f5eded1e8973e22ca0
Nikita Bubriak <31805409+nikitabubriak@users.noreply.github.com>
2022-05-17 23:52:14 +0300

    lab1
    create dockerfile, minikube kubernetes cluster, deployment, service

---
+++ micros/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace micros.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
